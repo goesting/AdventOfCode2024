e4e8a0c08745eb220059dc48677222f2a0906328
goesting <75796795+goesting@users.noreply.github.com>
2024-12-04 16:13:58 +0100

    Update Day04.rb

--- Day04.rb
+++ Day04.rb
@@ -6,4 +6,4 @@ p f.map.with_index{|line,y| line.map.with_index{|chr,x| [[1,0],[1,1],[0,1],[-1,1
 #PART2
 p f.map.with_index{|line,y|next if (y == 0 || y == f.size-1)
   line.map.with_index{|chr,x|next if (x==0 || x==f[0].size-1)
-    chr == ?A && [f.fetch(y-1).fetch(x-1),f[y+1][x+1]].sort.join == "MS" && [f[y+1][x-1],f[y-1][x+1]].sort.join == "MS"}}.flatten.compact.count{|c| c==true}
+    chr == ?A && [f.fetch(y-1).fetch(x-1),f[y+1][x+1]].sort.join == "MS" && [f[y+1][x-1],f[y-1][x+1]].sort.join == "MS"}}.flatten.compact.count{|c| c}
